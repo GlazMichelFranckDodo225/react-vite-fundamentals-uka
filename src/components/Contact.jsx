ba196d537b50bf4b9b7b90fd43e5df24238f0cfe
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-08-17 10:38:00 +0200

    Delete Demo.jsx and Create 4 Others Components : Contact.jsx, Footer.jsx, Header.jsx and Hero.jsx

---
+++ src/components/Contact.jsx
@@ -0,0 +1,10 @@
+const Contact = () => {
+  return (
+    <div>
+      <input type="text" />
+      <button type="submit">Submit</button>
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <div>
+      <div>
+        <ul>
+            <li>Home</li>
+            <li>Others</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Header
